eb45b565057d8e00e51c93350cec02c9a753e5e5
Adedokun Sheriff <156106821+Eminent97@users.noreply.github.com>
2024-11-25 11:58:02 +0100

    Add files via upload

---
+++ plp ass.py
@@ -0,0 +1,9 @@
+my_list = []
+my_list.extend([10,20,30, 40])
+my_list.insert(2, 15)
+my_list.extend([50,60,70])
+my_list.pop(7)
+sorted_list = sorted(my_list)
+#print(sorted_list)
+print(my_list[3])
+
